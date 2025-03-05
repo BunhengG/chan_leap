367da0ab845643559a54fc8f869400bc1eefba3e
BunhengG <bbmc255@gmail.com>
2025-03-05 19:26:53 +0700

    Update UI

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,14 @@ import "./globals.css";
 
 export const metadata = {
   title: "Seng Chanleap",
-  description: "Turning ideas into fast, functional, and beautiful digital solutions.",
+  description:
+    "Turning ideas into fast, functional, and beautiful digital solutions.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-background bg-[url('/texture_image.webp')] bg-cover bg-no-repeat bg-center sm:bg-fixed md:bg-scroll lg:bg-fixed">
+      <body className="bg-background bg-[url('/texture_image.webp')] bg-cover bg-no-repeat bg-center sm:bg-scroll md:bg-scroll lg:bg-fixed">
         <Navbar />
         {children}
       </body>
