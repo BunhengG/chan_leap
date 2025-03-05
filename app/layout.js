4646981279ace299112e86e9f84365f1ec1dd900
BunhengG <bbmc255@gmail.com>
2025-03-05 21:43:14 +0700

    Update UI

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,32 @@
+import { useEffect } from "react";
 import Navbar from "./pages/Navbar";
-import "./globals.css";
-
-export const metadata = {
-  title: "Seng Chanleap",
-  description:
-    "Turning ideas into fast, functional, and beautiful digital solutions.",
-};
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const handleIntersection = (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          document.body.style.backgroundImage = "url('/texture_image.webp')";
+          observer.disconnect();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      threshold: 0.1,
+    });
+
+    observer.observe(document.querySelector("body"));
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <html lang="en">
-      <body className="bg-background bg-[url('/texture_image.webp')] bg-cover bg-no-repeat bg-center sm:bg-scroll md:bg-scroll lg:bg-fixed">
+      <body className="bg-background sm:bg-cover sm:bg-no-repeat sm:bg-center lg:bg-fixed md:bg-scroll">
         <Navbar />
         {children}
       </body>
